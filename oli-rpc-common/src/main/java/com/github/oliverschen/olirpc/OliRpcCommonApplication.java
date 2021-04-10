6e0995cd6341bb7c64e2806ce76f157c42060dd8
chenkui <jihe12180@gmail.com>
2021-04-10 23:21:54 +0800

    add:common module

---
+++ oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/OliRpcCommonApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olirpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliRpcCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliRpcCommonApplication.class, args);
+    }
+
+}
---
+++ oli-rpc-common/src/test/java/com/github/oliverschen/olirpc/OliRpcCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olirpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliRpcCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
