fd9413ed19ab4ff0e6396cdac66b34016180e190
chenkui <jihe12180@gmail.com>
2021-04-11 18:01:19 +0800

    proxy module

---
+++ oli-rpc-proxy/src/main/java/com/github/oliverschen/OliRpcProxyApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliRpcProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliRpcProxyApplication.class, args);
+    }
+
+}
