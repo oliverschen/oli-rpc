77355d712356c50048278fb06656a6db95d25a64
chenkui <jihe12180@gmail.com>
2021-05-03 11:24:53 +0800

    demo project

---
+++ oli-rpc-demo/oli-rpc-consumer/src/main/java/com/github/oliverschen/olirpc/OliRpcComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olirpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliRpcComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliRpcComsumerApplication.class, args);
+    }
+
+}
---
+++ oli-rpc-demo/oli-rpc-provider/src/main/java/com/github/oliverschen/olirpc/OliRpcProviderApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olirpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliRpcProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliRpcProviderApplication.class, args);
+    }
+
+}
