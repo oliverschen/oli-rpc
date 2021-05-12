32bd19c54c29155fd74e720df01ef61ca79588b2
chenkui <jihe12180@gmail.com>
2021-05-12 22:42:53 +0800

    netty client handle

--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/client/NettyClientHandler.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/client/NettyClientHandler.java
@@ -2,60 +2,35 @@ package com.github.oliverschen.olirpc.remote.netty.client;
 
 import com.github.oliverschen.olirpc.response.OliResp;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.github.oliverschen.olirpc.remote.netty.client.OliRpcFuture.FUTURE;
 
 /**
  * netty client out bound handler
  *
  * @author ck
  */
-public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+public class NettyClientHandler extends SimpleChannelInboundHandler<OliResp> {
 
     private static final Logger log = LoggerFactory.getLogger(NettyClientHandler.class);
 
-    /**
-     * client 连接 server 完成立即执行
-     * @param ctx
-     * @throws Exception
-     */
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        log.debug("channel active complete：{}",ctx.channel());
-        super.channelActive(ctx);
-    }
-
-    @Override
-    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-         log.info("registered channel success");
-        super.channelRegistered(ctx);
-    }
 
     /**
-     * client 接收 server 发送的数据
-     *
-     * @param ctx
-     * @param msg 接收数据
-     * @throws Exception
+     * 读服务端数据
      */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        if (msg instanceof OliResp) {
-            OliResp resp = (OliResp) msg;
-            OliRpcFuture future = OliRpcFuture.obtain(resp.getId());
-            future.setResp(resp);
-            log.info("bind response to future complete");
-        }
-        ctx.writeAndFlush(msg);
+    protected void channelRead0(ChannelHandlerContext ctx, OliResp msg) {
+        log.info("response info :{}", msg);
+        OliRpcFuture future = FUTURE.get(msg.getId());
+        future.setResp(msg);
+        log.info("bind response to future complete");
     }
 
     /**
      * 发生异常时
-     *
-     * @param ctx
-     * @param cause
-     * @throws Exception
      */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
