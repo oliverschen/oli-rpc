f74e263edbea37830941abc3a8eeb193dab14082
chenkui <jihe12180@gmail.com>
2021-05-12 22:42:18 +0800

    netty client

--- oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/constant/Constants.java
+++ oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/constant/Constants.java
@@ -1,18 +1,25 @@
 package com.github.oliverschen.olirpc.constant;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @author ck
  */
 public final class Constants {
 
+    private Constants(){}
+
+
+
+
     public static final String DEFAULT_HOST = "http://localhost:";
     public static final String LOCAL_HOST = "localhost";
-
     public static final String JOINER = "@@";
-
 
     /**
      * netty 相关
      */
     public static final Integer NETTY_SERVER_DEFAULT_PORT = 6789;
+
+    public static final AtomicLong UNION_ID = new AtomicLong(0);
 }
--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/client/OliNetty.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/client/OliNetty.java
@@ -1,17 +1,22 @@
 package com.github.oliverschen.olirpc.remote.netty.client;
 
+import com.github.oliverschen.olirpc.codec.NettyDecoder;
+import com.github.oliverschen.olirpc.codec.NettyEncoder;
 import com.github.oliverschen.olirpc.remote.OliRpcClient;
 import com.github.oliverschen.olirpc.request.OliReq;
+import com.github.oliverschen.olirpc.response.OliResp;
+import com.github.oliverschen.olirpc.serialization.JsonSerialization;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpRequestEncoder;
-import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PreDestroy;
+
+import static com.github.oliverschen.olirpc.remote.netty.client.OliRpcFuture.FUTURE;
 
 /**
  * netty client
@@ -23,36 +28,49 @@ public class OliNetty implements OliRpcClient {
     private static final Logger log = LoggerFactory.getLogger(OliNetty.class);
     private Channel channel;
     private EventLoopGroup group;
+    private final String host;
+    private final Integer port;
 
-    public OliNetty() {
+    public static OliNetty init(String host, Integer port) {
+        return new OliNetty(host, port);
     }
 
     public OliNetty(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public OliNetty connect() {
         this.group = new NioEventLoopGroup();
         Bootstrap bootstrap = new Bootstrap()
                 .group(group)
-                .channel(SocketChannel.class)
-                .handler(new InitHandler());
-
-        ChannelFuture future = bootstrap.connect(host, port);
-        this.channel = future.channel();
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new NettyDecoder(OliResp.class, new JsonSerialization()));
+                        pipeline.addLast(new NettyEncoder(OliReq.class, new JsonSerialization()));
+                        pipeline.addLast(new NettyClientHandler());
+                    }
+                });
+        try {
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            channel = channelFuture.channel();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("oli-rpc netty client bind error:", e);
+        }
         log.info("netty client connect success");
+        return this;
     }
 
     @Override
-    public OliRpcFuture send(OliReq req) {
-        OliRpcFuture result = new OliRpcFuture(req);
-        result.put(req.getId());
-        channel.writeAndFlush(req).addListener((ChannelFutureListener) f -> {
-            if (f.isSuccess()) {
-                log.info("send data success");
-            }else {
-                f.channel().close();
-                result.completeExceptionally(f.cause());
-                log.error("send data error:", f.cause());
-            }
-        });
-        return result;
+    public OliResp send(OliReq req) {
+        FUTURE.put(req.getId(),new OliRpcFuture());
+        channel.writeAndFlush(req);
+        OliRpcFuture result = FUTURE.get(req.getId());
+        return result.getResp();
     }
 
 
