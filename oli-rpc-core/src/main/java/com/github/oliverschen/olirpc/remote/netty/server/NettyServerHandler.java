449d885fff428a1d56db2f59547b1037a7a7b259
chenkui <jihe12180@gmail.com>
2021-05-12 22:49:27 +0800

    netty server

--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/server/NettyServerHandler.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/server/NettyServerHandler.java
@@ -3,28 +3,25 @@ package com.github.oliverschen.olirpc.remote.netty.server;
 import com.github.oliverschen.olirpc.invoker.OliInvoker;
 import com.github.oliverschen.olirpc.request.OliReq;
 import com.github.oliverschen.olirpc.response.OliResp;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author ck
  */
-public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+public class NettyServerHandler extends SimpleChannelInboundHandler<OliReq> {
     private static final Logger log = LoggerFactory.getLogger(NettyServerHandler.class);
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        OliResp resp = null;
-        if (msg instanceof OliReq) {
-            OliReq req = (OliReq) msg;
-            log.debug("request data :{}", req.toString());
-            resp = OliInvoker.invoke(req);
-            log.info("invoke result:{}", resp.toString());
-        }
-        ctx.writeAndFlush(resp).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+    protected void channelRead0(ChannelHandlerContext ctx, OliReq msg) throws Exception {
+        OliResp resp;
+        log.debug("request data :{}", msg);
+        resp = OliInvoker.invoke(msg);
+        log.info("invoke result:{}", resp);
+        resp.setId(msg.getId());
+        ctx.writeAndFlush(resp);
     }
 
     @Override
--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/server/OliNettyServer.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/remote/netty/server/OliNettyServer.java
@@ -1,14 +1,21 @@
 package com.github.oliverschen.olirpc.remote.netty.server;
 
+import com.github.oliverschen.olirpc.codec.NettyDecoder;
+import com.github.oliverschen.olirpc.codec.NettyEncoder;
+import com.github.oliverschen.olirpc.request.OliReq;
+import com.github.oliverschen.olirpc.response.OliResp;
+import com.github.oliverschen.olirpc.serialization.JsonSerialization;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import javax.annotation.PreDestroy;
 
 import static com.github.oliverschen.olirpc.constant.Constants.NETTY_SERVER_DEFAULT_PORT;
 
@@ -21,31 +28,65 @@ public class OliNettyServer {
 
     private static final Logger log = LoggerFactory.getLogger(OliNettyServer.class);
 
+    private NioEventLoopGroup bossGroup;
+    private NioEventLoopGroup workGroup;
+
+    public static OliNettyServer init() {
+        return new OliNettyServer();
+    }
+
     /**
      * start oli-rpc netty server in port NETTY_SERVER_DEFAULT_PORT:6789
      */
     public void start() {
-
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        // 默认 work 线程数量是 CPU 核心数 * 2
-        NioEventLoopGroup workGroup = new NioEventLoopGroup();
         ServerBootstrap bootstrap = new ServerBootstrap()
-                .group(bossGroup, workGroup)
+                .group(bossGroup(), workGroup())
+                .option(ChannelOption.SO_BACKLOG, 1024)
                 .channel(NioServerSocketChannel.class)
-                .handler(new NettyServerHandler());
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new NettyEncoder(OliResp.class, new JsonSerialization()));
+                        pipeline.addLast(new NettyDecoder(OliReq.class, new JsonSerialization()));
+                        pipeline.addLast(new NettyServerHandler());
+                    }
+                });
         try {
-            String hostAddress = InetAddress.getLocalHost().getHostAddress();
-            Channel channel = bootstrap.bind(hostAddress, NETTY_SERVER_DEFAULT_PORT).sync().channel();
-            channel.closeFuture().sync();
-        } catch (InterruptedException | UnknownHostException e) {
-            e.printStackTrace();
+            log.info("oli-rpc netty server starting");
+            ChannelFuture channelFuture = bootstrap.bind(NETTY_SERVER_DEFAULT_PORT).sync();
+            log.info("oli-rpc netty server started");
+            // 阻塞，等待关闭事件才会关闭服务端
+            channelFuture.channel().closeFuture().sync();
+        } catch ( InterruptedException e) {
             log.error("oli-rpc netty server start error:", e);
             Thread.currentThread().interrupt();
-        }finally {
-            workGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-            log.debug("oli-rpc bossGroup and work Group shutdown");
         }
+    }
+
+    /**
+     * workerGroup
+     */
+    private EventLoopGroup workGroup() {
+        // 默认 work 线程数量是 CPU 核心数 * 2
+        workGroup = new NioEventLoopGroup();
+        return workGroup;
+    }
+
+    /**
+     * bossGroup
+     */
+    private EventLoopGroup bossGroup() {
+        bossGroup = new NioEventLoopGroup(1);
+        return bossGroup;
+    }
+
+    @PreDestroy
+    public void close() {
+        bossGroup.shutdownGracefully();
+        workGroup.shutdownGracefully();
+        log.info("oli-rpc netty server was stopped");
     }
 
 
