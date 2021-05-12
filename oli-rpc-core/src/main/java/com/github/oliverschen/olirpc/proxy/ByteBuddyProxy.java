b4ed07ea37a250585fa6bac0f137093248c2f455
chenkui <jihe12180@gmail.com>
2021-05-12 22:48:28 +0800

    动态代理修改

--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/proxy/ByteBuddyProxy.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/proxy/ByteBuddyProxy.java
@@ -1,17 +1,21 @@
 package com.github.oliverschen.olirpc.proxy;
 
-import com.github.oliverschen.olirpc.remote.http.RemoteOkHttp;
+import com.github.oliverschen.olirpc.remote.netty.client.OliNetty;
+import com.github.oliverschen.olirpc.request.OliReq;
 import com.github.oliverschen.olirpc.response.OliResp;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.Origin;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static com.github.oliverschen.olirpc.constant.Constants.NETTY_SERVER_DEFAULT_PORT;
 import static com.github.oliverschen.olirpc.util.JsonUtil.MAPPER;
 import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
 
@@ -19,6 +23,8 @@ import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
  * @author ck
  */
 public class ByteBuddyProxy<T,X> {
+
+    private static final Logger log = LoggerFactory.getLogger(ByteBuddyProxy.class);
 
     private String url;
     private Class<X> result;
@@ -61,7 +67,12 @@ public class ByteBuddyProxy<T,X> {
         public Object intercept(@AllArguments Object[] allArguments,
                                 @Origin Method method) throws IOException {
             // intercept any method of any signature
-            OliResp oliResp = RemoteOkHttp.post(buildOliReq(serviceClass,method,allArguments), url);
+//            OliResp oliResp = RemoteOkHttp.post(buildOliReq(serviceClass,method,allArguments), url);
+            OliReq req = buildOliReq(serviceClass, method, allArguments);
+            log.info("动态代理 invoke 信息：{}", req);
+            OliResp oliResp = OliNetty.init(url, NETTY_SERVER_DEFAULT_PORT)
+                    .connect()
+                    .send(req);
             return MAPPER.readValue(oliResp.getData().toString(),this.result);
         }
     }
--- oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/proxy/JdkProxy.java
+++ oli-rpc-core/src/main/java/com/github/oliverschen/olirpc/proxy/JdkProxy.java
@@ -1,11 +1,15 @@
 package com.github.oliverschen.olirpc.proxy;
 
-import com.github.oliverschen.olirpc.remote.http.RemoteOkHttp;
+import com.github.oliverschen.olirpc.remote.netty.client.OliNetty;
+import com.github.oliverschen.olirpc.request.OliReq;
 import com.github.oliverschen.olirpc.response.OliResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+import static com.github.oliverschen.olirpc.constant.Constants.NETTY_SERVER_DEFAULT_PORT;
 import static com.github.oliverschen.olirpc.util.JsonUtil.MAPPER;
 
 /**
@@ -14,6 +18,8 @@ import static com.github.oliverschen.olirpc.util.JsonUtil.MAPPER;
  * @author ck
  */
 public class JdkProxy<T,X> extends AbstractBaseProxy implements InvocationHandler {
+    private static final Logger log = LoggerFactory.getLogger(JdkProxy.class);
+
     private Class<T> serviceClass;
     private String url;
     private Class<X> result;
@@ -28,7 +34,15 @@ public class JdkProxy<T,X> extends AbstractBaseProxy implements InvocationHandle
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        OliResp oliResp = RemoteOkHttp.post(buildOliReq(serviceClass,method,args), url);
-        return MAPPER.readValue(oliResp.getData().toString(),this.result);
+        OliReq req = buildOliReq(serviceClass, method, args);
+        log.info("动态代理 invoke 信息：{}", req);
+        OliResp oliResp = OliNetty.init(url, NETTY_SERVER_DEFAULT_PORT)
+                .connect()
+                .send(req);
+
+        // http 请求，todo 后面需要写成自适应的方式来调用
+//        OliResp oliResp = RemoteOkHttp.post(buildOliReq(serviceClass, method, args), url);
+
+        return oliResp != null ? MAPPER.readValue(oliResp.getData().toString(), this.result) : null;
     }
 }
