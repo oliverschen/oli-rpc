607d8d0cde3c56b67f6ae2059bd0ec4a844754a0
chenkui <jihe12180@gmail.com>
2021-05-16 00:25:58 +0800

    增加枚举

--- oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/constant/Enums.java
+++ oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/constant/Enums.java
@@ -1,7 +1,5 @@
 package com.github.oliverschen.olirpc.constant;
 
-import com.github.oliverschen.olirpc.exception.OliException;
-
 import java.util.Arrays;
 
 /**
@@ -10,9 +8,12 @@ import java.util.Arrays;
 public class Enums {
 
     public enum ProxyType{
-        // 代理类型，默认 JDK Proxy
+        // 代理类型，默认 JDK Proxy，已实现（JDK,BYTE_BUDDY）
         JDK,
         BYTE_BUDDY,
+        JAVA_SSIST,
+        GCLIB,
+        ASPECT_J,
         ;
         public static ProxyType of(String type) {
             return Arrays.stream(ProxyType.values()).filter(p -> p.name().equals(type))
