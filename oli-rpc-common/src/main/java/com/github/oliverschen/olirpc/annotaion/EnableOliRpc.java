d595e913bdd063c938fe6369da622224a6e0936d
chenkui <jihe12180@gmail.com>
2021-05-25 23:34:44 +0800

    annotation

--- oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/annotaion/EnableOliRpc.java
+++ oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/annotaion/EnableOliRpc.java
@@ -1,10 +1,17 @@
 package com.github.oliverschen.olirpc.annotaion;
 
+import java.lang.annotation.*;
+
 /**
  * 依赖 Spring 开启服务注解
  *
  * @author ck
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface EnableOliRpc {
+
 
 }
