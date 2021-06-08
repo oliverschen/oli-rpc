efb1d01d6345665e7fae3c92d9f20ba5f2ff6e51
chenkui <jihe12180@gmail.com>
2021-06-08 23:21:08 +0800

    统一参数协议

---
+++ oli-rpc-common/src/main/java/com/github/oliverschen/olirpc/protocol/OliUrl.java
@@ -0,0 +1,77 @@
+package com.github.oliverschen.olirpc.protocol;
+
+/**
+ * 服务寻址协议
+ * http://192.168.0.104:17828
+ * @author ck
+ */
+public class OliUrl<T> {
+
+    /**
+     * 原始 url
+     */
+    private String srcUrl;
+
+    private String host;
+    private Integer port;
+
+    /**
+     * 协议：netty/http
+     */
+    private String protocol;
+
+    /**
+     * 限定名
+     */
+    private Class<T> serviceClass;
+
+    private String proxy;
+
+    public String getProxy() {
+        return proxy;
+    }
+
+    public void setProxy(String proxy) {
+        this.proxy = proxy;
+    }
+
+    public String getSrcUrl() {
+        return srcUrl;
+    }
+
+    public void setSrcUrl(String srcUrl) {
+        this.srcUrl = srcUrl;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public Class<T> getServiceClass() {
+        return serviceClass;
+    }
+
+    public void setServiceClass(Class<T> serviceClass) {
+        this.serviceClass = serviceClass;
+    }
+}
