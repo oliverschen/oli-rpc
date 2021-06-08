0d4c0466a2f6e3b87e7f6935150d391a85d44ef3
chenkui <jihe12180@gmail.com>
2021-06-08 23:21:46 +0800

    cluster实现

--- oli-rpc-remote/src/main/java/com/github/oliverschen/olirpc/remote/refer/OliReferHub.java
+++ oli-rpc-remote/src/main/java/com/github/oliverschen/olirpc/remote/refer/OliReferHub.java
@@ -1,10 +1,11 @@
 package com.github.oliverschen.olirpc.remote.refer;
 
+import com.github.oliverschen.olirpc.Cluster;
 import com.github.oliverschen.olirpc.LoadBalance;
 import com.github.oliverschen.olirpc.Router;
-import com.github.oliverschen.olirpc.properties.OliProperties;
-import com.github.oliverschen.olirpc.remote.proxy.OliProxy;
+import com.github.oliverschen.olirpc.protocol.OliUrl;
 import com.github.oliverschen.olirpc.registry.redis.RedisRegister;
+import com.github.oliverschen.olirpc.remote.proxy.OliProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +25,11 @@ public class OliReferHub {
     @Autowired
     private RedisRegister redisRegister;
     @Autowired
-    private OliProperties oliProperties;
-    @Autowired
     private Router router;
     @Autowired
     private LoadBalance loadBalance;
+    @Autowired
+    private Cluster cluster;
 
     /**
      * 随机获取注册中心服务
@@ -39,9 +40,11 @@ public class OliReferHub {
         List<String> route = router.route(services);
         // load balance
         String url = loadBalance.balance(route);
+        // cluster
+        OliUrl oliUrl = cluster.obtainOliUrl(url,serviceClass);
+
         log.info("random service url is :{}", url);
-        Object o = OliProxy.init(oliProperties).create(serviceClass, url);
-        return (T) o;
+        return OliProxy.init().create(oliUrl);
     }
 
 }
