d686980cfbfae9debfdfba9a7d81c318d2949a97
chenkui <jihe12180@gmail.com>
2021-07-22 23:27:16 +0800

    weight lb

--- oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/loadbalance/RandomLB.java
+++ oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/loadbalance/RandomLB.java
@@ -3,15 +3,20 @@ package com.github.oliverschen.olirpc.loadbalance;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.github.oliverschen.olirpc.constant.Constants.JOINER;
 
 /**
  * @author ck
  */
-@Component
+@Component("randomLB")
 public class RandomLB extends AbstractLoadBalance {
 
     @Override
     public String balance(List<String> list) {
-        return super.balance(list);
+        String service = list.get(ThreadLocalRandom.current().nextInt(list.size()));
+        String[] split = service.split(JOINER);
+        return split[0];
     }
 }
---
+++ oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/loadbalance/WeightLB.java
@@ -0,0 +1,20 @@
+package com.github.oliverschen.olirpc.loadbalance;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 按照权重选择服务
+ * @author ck
+ */
+@Component("weightLB")
+public class WeightLB extends AbstractLoadBalance {
+
+    @Override
+    public String balance(List<String> service) {
+        // todo 按照权重进行选择服务
+        return service.get(0);
+    }
+
+}
