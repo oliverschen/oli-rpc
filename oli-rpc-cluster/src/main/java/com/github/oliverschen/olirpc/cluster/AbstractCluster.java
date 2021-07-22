9c2b4c2ff3dd7e9f61a25579c77b608a85c127c7
chenkui <jihe12180@gmail.com>
2021-07-22 23:12:22 +0800

    default obtain url method

--- oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/cluster/AbstractCluster.java
+++ oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/cluster/AbstractCluster.java
@@ -1,10 +1,38 @@
 package com.github.oliverschen.olirpc.cluster;
 
 import com.github.oliverschen.olirpc.Cluster;
+import com.github.oliverschen.olirpc.exception.OliException;
+import com.github.oliverschen.olirpc.properties.OliProperties;
+import com.github.oliverschen.olirpc.protocol.OliUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import static com.github.oliverschen.olirpc.constant.Constants.*;
 
 /**
  * @author ck
  */
-public abstract class AbstractCluster implements Cluster{
+public abstract class AbstractCluster implements Cluster {
+
+    @Autowired
+    private OliProperties oliProperties;
+
+    @Override
+    public  <T> OliUrl<T> obtainOliUrl(String url, Class<T> serviceClass) {
+        OliUrl<T> oliUrl = new OliUrl<>();
+        oliUrl.setSrcUrl(url);
+        if (StringUtils.hasLength(url)) {
+            String[] address = url.split(URL_SPLIT)[1].split(URL_COLON);
+            if (address.length != SPLIT_SIZE) {
+                throw new OliException("配置有误");
+            }
+            oliUrl.setHost(address[0]);
+            oliUrl.setPort(Integer.parseInt(address[1]));
+        }
+
+        oliUrl.setProtocol(oliProperties.getProtocol());
+        oliUrl.setServiceClass(serviceClass);
+        return oliUrl;
+    }
 
 }
--- oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/cluster/OliUrlCluster.java
+++ oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/cluster/OliUrlCluster.java
@@ -1,13 +1,7 @@
 package com.github.oliverschen.olirpc.cluster;
 
-import com.github.oliverschen.olirpc.exception.OliException;
-import com.github.oliverschen.olirpc.properties.OliProperties;
 import com.github.oliverschen.olirpc.protocol.OliUrl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
-import static com.github.oliverschen.olirpc.constant.Constants.*;
 
 /**
  * @author ck
@@ -15,24 +9,11 @@ import static com.github.oliverschen.olirpc.constant.Constants.*;
 @Component
 public class OliUrlCluster extends AbstractCluster {
 
-    @Autowired
-    private OliProperties oliProperties;
-
+    /**
+     * 调用默认的父类方法
+     */
     @Override
     public <T> OliUrl<T> obtainOliUrl(String url,Class<T> serviceClass) {
-        OliUrl<T> oliUrl = new OliUrl<>();
-        oliUrl.setSrcUrl(url);
-        if (StringUtils.hasLength(url)) {
-            String[] address = url.split(URL_SPLIT)[1].split(URL_COLON);
-            if (address.length != SPLIT_SIZE) {
-                throw new OliException("配置有误");
-            }
-            oliUrl.setHost(address[0]);
-            oliUrl.setPort(Integer.parseInt(address[1]));
-        }
-
-        oliUrl.setProtocol(oliProperties.getProtocol());
-        oliUrl.setServiceClass(serviceClass);
-        return oliUrl;
+        return super.obtainOliUrl(url, serviceClass);
     }
 }
