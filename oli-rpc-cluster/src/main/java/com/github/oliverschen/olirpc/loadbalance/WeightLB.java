876a0a43c3e887f0e8c20d134319a206eec5d64f
chenkui <jihe12180@gmail.com>
2021-08-12 00:33:18 +0800

    nothing

--- oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/loadbalance/WeightLB.java
+++ oli-rpc-cluster/src/main/java/com/github/oliverschen/olirpc/loadbalance/WeightLB.java
@@ -1,5 +1,7 @@
 package com.github.oliverschen.olirpc.loadbalance;
 
+import com.github.oliverschen.olirpc.exception.OliException;
+
 import java.util.List;
 
 /**
@@ -11,6 +13,9 @@ public class WeightLB extends AbstractLoadBalance {
     @Override
     String doBalance(List<String> service) {
         // todo 按照权重进行选择服务
+        if (service.isEmpty()) {
+            throw new OliException("provider is empty");
+        }
         return service.get(0);
     }
 
