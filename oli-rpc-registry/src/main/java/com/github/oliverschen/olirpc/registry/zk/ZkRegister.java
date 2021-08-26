c86cec747355e66f74fcc72fd4a497b206be57ab
chenkui <jihe12180@gmail.com>
2021-08-26 21:55:07 +0800

    add todo

--- oli-rpc-registry/src/main/java/com/github/oliverschen/olirpc/registry/zk/ZkRegister.java
+++ oli-rpc-registry/src/main/java/com/github/oliverschen/olirpc/registry/zk/ZkRegister.java
@@ -12,16 +12,18 @@ public class ZkRegister implements Register {
 
     @Override
     public void register(Object bean) {
-
+        // todo zk 注册中心
     }
 
     @Override
     public Set<String> obtainServices(String serviceKey) {
+        // todo 获取服务
         return null;
     }
 
     @Override
     public Register newClient(OliProperties oliProperties) {
+        // todo 创建连接
         return null;
     }
 }
